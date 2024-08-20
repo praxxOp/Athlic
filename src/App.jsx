42fb74cea6d62a5af2f37a0b29019b820caef9bc
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-08-20 18:32:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Frames from './Components/Frames/Frames'
+
+const App = () => {
+  return (
+    <>
+    <Frames/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Frames/Frames.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./Frames.css";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const Frames = () => {
+  const canvasRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const maxIndex = 121;
+  const imagesRef = useRef([]);
+
+  const { scrollYProgress } = useScroll();
+  const animatedIndex = useTransform(scrollYProgress, [0, 1], [1, maxIndex]);
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+
+    const preloadImages = () => {
+      for (let index = 1; index <= maxIndex; index++) {
+        const imageUrl = `/Assets/Extracted-Frames/frame_${index
+          .toString()
+          .padStart(4, "0")}.jpeg`;
+        const img = new Image();
+        img.src = imageUrl;
+        img.onload = () => {
+          imagesRef.current[index] = img;
+          setImagesLoaded((prev) => {
+            const newCount = prev + 1;
+            if (newCount === maxIndex) {
+              loadImage(currentIndex, context);
+            }
+            return newCount;
+          });
+        };
+      }
+    };
+    preloadImages();
+  }, [currentIndex]);
+
+  const loadImage = (index, context) => {
+    if (index >= 1 && index <= maxIndex) {
+      const img = imagesRef.current[index];
+      if (img) {
+        canvasRef.current.width = window.innerWidth;
+        canvasRef.current.height = window.innerHeight;
+        const scaleX = canvasRef.current.width / img.width;
+        const scaleY = canvasRef.current.height / img.height;
+        const scale = Math.max(scaleX, scaleY);
+        const newWidth = img.width * scale;
+        const newHeight = img.height * scale;
+        const offsetX = (canvasRef.current.width - newWidth) / 2;
+        const offsetY = (canvasRef.current.height - newHeight) / 2;
+        context.imageSmoothingEnabled = true;
+        context.imageSmoothingQuality = "high";
+        context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+        context.drawImage(img, offsetX, offsetY, newWidth, newHeight);
+      }
+    }
+  };
+
+  useEffect(() => {
+    return animatedIndex.onChange((latestIndex) => {
+      loadImage(Math.floor(latestIndex), canvasRef.current.getContext("2d"));
+    });
+  }, [animatedIndex]);
+
+  return (
+    <div className="Frames__parent">
+      <motion.div className="Frames__children" style={{ height: "100vh" }}>
+        <canvas ref={canvasRef} id="FrameImages"></canvas>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Frames;
