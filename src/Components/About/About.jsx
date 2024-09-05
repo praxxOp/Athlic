3a421f2cbbbc7deb1a2937c5d6764ba6f22d607c
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-09-05 23:59:00 +0530

    modified:   src/Components/About/About.css 	modified:   src/Components/About/About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,8 +6,14 @@ const About = () => {
       <div className="About__main">
         <div className="stats__section">
           <div className="stats__section__up">
-            <div className="stats__section__left"></div>
-            <div className="stats__section__right"></div>
+            <div className="stats__section__left">
+              <h4>Amenities Managed.</h4>
+              <h1>500+</h1>
+            </div>
+            <div className="stats__section__right">
+              <h4>Facility Bookings Per Month.</h4>
+              <h1>1000+</h1>
+            </div>
           </div>
           <div className="stats__section__down">
             <div
@@ -15,13 +21,19 @@ const About = () => {
               style={{
                 marginTop: "-35.5vh",
               }}
-            ></div>
+            >
+              <h4>User Satisfaction.</h4>
+              <h1>99%</h1>
+            </div>
             <div
               className="stats__section__left"
               style={{
                 marginTop: "0%",
               }}
-            ></div>
+            >
+              <h4>Sports Supported.</h4>
+              <h1>10+</h1>
+            </div>
           </div>
         </div>
       </div>
