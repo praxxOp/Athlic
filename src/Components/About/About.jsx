00a69fded5083d7c6b24e28d8a22088f82bf46ee
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-09-05 21:06:39 +0530

    About component Added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 const About = () => {
   return (
     <>
-    <div className="About__main">
-        
-    </div>
+      <div className="About__main">
+        <div className="stats__section">
+          <div className="stats__section__up">
+            <div className="stats__section__left"></div>
+            <div className="stats__section__right"></div>
+          </div>
+          <div className="stats__section__down">
+            <div
+              className="stats__section__right"
+              style={{
+                marginTop: "-35.5vh",
+              }}
+            ></div>
+            <div
+              className="stats__section__left"
+              style={{
+                marginTop: "0%",
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
