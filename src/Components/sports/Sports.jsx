83630c5e94ce62813d4595a3faffdb6651270d67
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-09-14 18:09:52 +0530

    React-Router added

---
+++ src/Components/sports/Sports.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sports.css";
+const Sports = () => {
+  return (
+    <>
+      <div className="sports__main">
+        hiiihi
+      </div>
+
+    </>
+  );
+};
+
+export default Sports;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import Sports from './Components/sports/Sports.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path:"/hi",
+    element : <Sports/>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
