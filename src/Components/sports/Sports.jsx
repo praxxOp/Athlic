b8c57bd55baeba22dbf2214d5e7abc9081df1210
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-09-16 16:35:31 +0530

    modified:   src/Components/sports/Sports.css 	modified:   src/Components/sports/Sports.jsx

--- src/Components/sports/Sports.jsx
+++ src/Components/sports/Sports.jsx
@@ -1,26 +1,46 @@
 import React from "react";
 import "./Sports.css";
 const Sports = () => {
-  
+  const sports = [
+    {
+      name: "Swimming",
+    },
+    {
+      name: "Cricket",
+    },
+    {
+      name: "volleyballl",
+    },
+    {
+      name: "Gymnastics",
+    },
+  ];
   return (
     <>
       <div className="sports__main">
         <div className="sports__main__heading">
-            <h1>SPORTS</h1>
-            <h2>Supported Sports.</h2>
+          <h1>SPORTS</h1>
+          <h2>Supported Sports.</h2>
         </div>
         <div className="sports__main__GroupSection">
-            <div className="sports__main__GroupSection__left">
-              <div className="GroupSection__left__container">
-
-              </div>
-               </div>
-            <div className="sports__main__GroupSection__right">
-
+          <div className="sports__main__GroupSection__left">
+            <div className="GroupSection__left__container">
+              <h1>football</h1>
             </div>
+          </div>
+          <div className="sports__main__GroupSection__right">
+            {sports.map((obj) => {
+              return (
+                <div className="GroupSection__right__containers">
+                  <div className="GroupSection__right__containers__heading">
+                    <h1>{obj.name}</h1>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-
     </>
   );
 };
