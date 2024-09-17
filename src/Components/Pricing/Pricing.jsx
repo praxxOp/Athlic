11f9ce2b38f07e9a356205feebf7c230b58c2745
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-09-17 18:54:34 +0530

    modified:   src/Components/Pricing/Pricing.jsx

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Pricing.css";
+import ScrollToTop from "../ScrollToTop"
 const Pricing = () => {
   return (
     <>
+    <ScrollToTop/>
       <div className="pricing__main">
       <div className="pricing__main__heading">
           <h1>PRICING</h1>
