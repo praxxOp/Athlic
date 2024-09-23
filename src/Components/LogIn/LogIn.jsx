605d21bd94221ff981ba67e1b7e3544a06dfbee6
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-09-23 21:50:42 +0530

    modified:   src/Components/LogIn/LogIn.css 	modified:   src/Components/LogIn/LogIn.jsx

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./LogIn.css";
 import Building from "/Assets/black_building.gif"
+import { motion } from "framer-motion";
 const LogIn = () => {
   return (
     <>
@@ -8,7 +9,18 @@ const LogIn = () => {
         <div className="Login__main__video">
         <img src={Building} alt="Building GIF" />
         </div>
-        <div className="Login__main__right"></div>
+        <div className="Login__main__right">
+            <div className="Login__main__right__heading">
+                <h1>Access</h1>
+                <h3>Your Dashboard</h3>
+            </div>
+            <div className="Login__main__right__button">
+                <motion.h1
+                whileHover={{
+                    backgroundColor : "aliceblue",
+                }} >SignIn With Google</motion.h1>
+            </div>
+        </div>
       </div>
     </>
   );
