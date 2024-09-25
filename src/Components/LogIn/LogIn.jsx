6368ee121e59d89f0c4e27415a3c1037663d1517
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-09-25 13:54:16 +0530

    deleted:    public/Assets/ColorFullGif.gif 	new file:   public/Assets/ColorFullGif.mp4 	modified:   src/Components/LogIn/LogIn.css 	modified:   src/Components/LogIn/LogIn.jsx

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./LogIn.css";
-import Building from "/Assets/ColorFullGif.gif"
+import Building from "/Assets/ColorFullGif.mp4"
 import { motion } from "framer-motion";
 const LogIn = () => {
   return (
     <>
       <div className="Login__main">
         <div className="Login__main__video">
-        <img src={Building} alt="Building GIF" />
+        <video src= {Building} autoPlay loop muted ></video>
         <h2>Athlic Corporation © 2024</h2>
         </div>
         <div className="Login__main__right">
