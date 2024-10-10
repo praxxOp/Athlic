01410ebb28aa8d00f28cf74c693b594c5a590b6e
Prathamesh <shelke.prathamesh0012@gmail.com>
2024-10-10 16:00:53 +0530

    modified:   src/Components/Dashboard/Card/Card.css 	modified:   src/Components/Dashboard/Card/Card.jsx 	modified:   src/Components/sports/Sports.css

--- src/Components/Dashboard/Card/Card.jsx
+++ src/Components/Dashboard/Card/Card.jsx
@@ -4,6 +4,7 @@ import "./Card.css";
 const Card = () => {
   const cardobj = {
     name: "Hiranandani Society",
+    address : "kopri Colony, Siddharth thane(East)"
   };
 
   return (
@@ -12,6 +13,12 @@ const Card = () => {
         <div className="Account-Card-Heading">
           <h1>{cardobj.name}</h1>
         </div>
+        <div className="Account-Card-details">
+        <p>{cardobj.address}.</p>
+        </div>
+       <div className="Account-Card-button">
+        <h3>View Profile</h3>
+       </div>
       </div>
     </>
   );
